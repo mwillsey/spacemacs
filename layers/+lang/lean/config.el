e1e4c4b691ddf170318b9617dcea137c3cfa87b4
Max Willsey <me@mwillsey.com>
2017-12-13 10:29:55 -0800

    Add layer for lean
    Mostly from https://github.com/jroesch/layer-lean

---
+++ layers/+lang/lean/config.el
@@ -0,0 +1,13 @@
+;;; config.el --- lean Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Max Willsey
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar lean-rootdir "/usr/local"
+  "Prefix of lean installation that contains bin, lib, and so on.")
---
+++ layers/+lang/lean/packages.el
@@ -0,0 +1,51 @@
+;;; packages.el --- Lean Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Max Willsey
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst lean-packages
+  '(lean-mode
+    company-lean
+    helm-lean
+    ))
+
+(defun lean/init-lean-mode ()
+  (use-package lean-mode
+    :defer t
+    :mode ("\\.lean\\'" . lean-mode)
+    :config
+    (progn
+
+      (spacemacs/declare-prefix-for-mode 'lean-mode "mc" "compile")
+      (spacemacs/declare-prefix-for-mode 'lean-mode "mh" "documentation")
+      (spacemacs/declare-prefix-for-mode 'lean-mode "mp" "process")
+      (spacemacs/declare-prefix-for-mode 'lean-mode "mg" "tags")
+      (spacemacs/declare-prefix-for-mode 'lean-mode "mT" "toggles")
+
+      (spacemacs/set-leader-keys-for-major-mode 'lean-mode
+       "cc" 'lean-std-exe
+       "pr" 'lean-server-restart
+       ;; "pf" 'lean-set-option
+       "hh" 'lean-eldoc-documentation-function
+       "ht" 'lean-show-type
+       ;; "hg" 'lean-show-goal-at-pos
+       "hk" 'quail-show-key
+       ;; "hi" 'lean-show-id-keyword-info
+       "f" 'lean-fill-placeholder
+       ;; "gu" 'lean-generate-tags
+       "gg" 'lean-find-definition
+       ;; these aren't really ui toggles, they show/hide windows
+       "?" 'lean-toggle-show-goal
+       "e" 'lean-toggle-next-error
+       "Tm" 'lean-message-boxes-toggle
+       "SPC" 'lean-hole
+       ))))
+
+(defun lean/init-company-lean ())
+(defun lean/init-helm-lean ())
